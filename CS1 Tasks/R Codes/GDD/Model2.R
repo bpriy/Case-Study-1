b1bf0b88213b8f896352a60c20858cd2627fa893
bpriy <98420978+bpriy@users.noreply.github.com>
2022-02-18 14:58:04 -0500

    commit 3

--- CS1 Tasks/R Codes/GDD/Model2.R
+++ CS1 Tasks/R Codes/GDD/Model2.R
@@ -37,9 +37,59 @@ clipr::write_clip(kyoto_ts)
 kdat_list = split(kyoto_ts, kyoto_ts$year)
 
 x = 1
-z = 1
 totyears = 1951:2022
 y = length(totyears)
+
+# part 1
+CR = -100
+  for (TC in seq(7,12)){
+    z = 1
+    for (i in 1:y){
+      Cd <- with(kdat_list[[i]], ifelse(0 <= TC & TC <= tmin & tmin <= tmax, 0, 
+                                        ifelse(0 <= tmin & tmin <= TC & TC <= tmax,((tavg - tmin)-((tmax - TC)/2)),
+                                               ifelse(0 <= tmin & tmin <= tmax & tmax <= TC, (tavg - tmin),
+                                                      ifelse(tmin <= 0 & 0 <= tmax & tmax <= TC, ((tmax)/(tmax-tmin))*(tmax/2),
+                                                             (((tmax)/(tmax-tmin))*(tmax/2)-((tmax-TC)/2)))))))
+      Cd <- with(kdat_list[[i]], ifelse(Cd <=0, Cd, Cd*(-1)))
+      nky <- cbind(Cd,kdat_list[[i]])
+      nky[,"cm_cd"] <- cumsum(nky$Cd)
+      nky
+      chill <- min(which(nky$cm_cd <= CR))  # number of chill days
+      if (!is.finite(chill)) {
+        next
+      }
+      hs = chill + 1
+      nky$Cd[hs:length(nky$Cd)] <- 0 
+      nky$cm_cd[hs:length(nky$cm_cd)] <- 0 
+      
+      nky[,"Ca"] <- with(kdat_list[[i]], ifelse(0 <= TC & TC <= tmin & tmin <= tmax, tavg-TC, 
+                                                ifelse(0 <= tmin & tmin <= TC & TC <= tmax,(tmax - TC)/2,
+                                                       ifelse(0 <= tmin & tmin <= tmax & tmax <= TC, 0,
+                                                              ifelse(tmin <= 0 & 0 <= tmax & tmax <= TC, 0,
+                                                                     (tmax - TC)/2))))) 
+      
+      nky$Ca[1:chill] <- 0 
+      nky[,"cm_ca"] <- cumsum(nky$Ca)
+      
+      heat <- min(which(CR + nky$cm_ca  >= 0))  # number of heat days
+      totdays <- chill + heat
+      
+      ppd[z] = c(totdays)
+      year[z] = kdat_list[[i]][1,5]
+      
+      print(i)
+      print(TC)
+      print(CR)
+    }
+
+  }
+##################################################
+
+predpbd <- data.frame(year,ppd)
+kyoto_chpb <- left_join(predpbd, publicpbd, by ="year") %>% 
+  rowwise() %>% mutate(diff2 = (bloom_doy-ppd)^2)
+z = z + 1
+
 
 for (CR in -100: -200){
   for (TC in seq(7,12)){
